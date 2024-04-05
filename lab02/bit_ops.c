d31b3d46d718849474246a09a2b84c6dbdc1f175
KodomoHan <2821117052@qq.com>
2024-04-05 17:40:34 +0800

    finished e0-e2

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -3,25 +3,38 @@
 
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
-unsigned get_bit(unsigned x,
-                 unsigned n) {
+unsigned get_bit(unsigned x,unsigned n) {
     // YOUR CODE HERE
+
+    //100'1'000->100'1'
+    //100'1'&000001=>'1'
+
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    return (x>>n)&1u;
 }
+
+
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
+void set_bit(unsigned * x,unsigned n,unsigned v) {
     // YOUR CODE HERE
+    //1.clear the nth bit first
+    //1111 '1'111-> 1111 '0'111 
+    //1111 '0'111-> 1111 '0'111
+    //2. change the nth bit to v using '|'
+    //1111 '0'111 | 0000 '1'000 =>1111 '1'111
+    //1111 '0'111 | 0000 '0'000 =>1111 '0'111
+    
+    *x=(*x&~(1u<<n))|(v<<n);
+
+    
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
+void flip_bit(unsigned * x,unsigned n) {
     // YOUR CODE HERE
+    *x=*x^(1u<<n);
 }
 
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,18 @@
 #include <string.h>
 #include "lfsr.h"
 
+
+uint16_t get_next_MSB(uint16_t *reg){
+     uint16_t MSB = *reg & 1;
+     MSB ^= (*reg >> 2) & 1;
+     MSB ^= (*reg >> 3) & 1;
+     MSB ^= (*reg >> 5) & 1;
+     return MSB;
+}
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    (*reg)>>=1;
+    (*reg)|=(get_next_MSB(reg)<<15);
 }
 
